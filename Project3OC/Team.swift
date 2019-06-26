8d007df10f8c0df7d560c08b4f35364c2046eb11
vinceled <mark.ledoux@live.fr>
2019-06-26 17:51:17 +0200

    final commit for June 26

--- Project3OC/Team.swift
+++ Project3OC/Team.swift
@@ -2,16 +2,12 @@
 //  Team.swift
 //  Project3OC
 //
-//  Created by Mark LEDOUX on 05/06/2019.
+//  Created by Mark LEDOUX on 26/06/2019.
 //  Copyright © 2019 Mark LEDOUX. All rights reserved.
 //
 
-import Foundation
+import Cocoa
 
 class Team {
-    let team = [Character]()
-
-    func createTeam() {
-        print(team)
-    }
+   
 }
