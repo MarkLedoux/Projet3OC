0f1f2ad0340f27dff059a899a6e4c493bd171621
vinceled <mark.ledoux@live.fr>
2019-06-27 07:09:14 +0200

    create function to make the teams

--- Project3OC/Team.swift
+++ Project3OC/Team.swift
@@ -9,5 +9,13 @@
 import Cocoa
 
 class Team {
-   
+   var team = [Character]()
+    func createTeam() {
+        repeat {
+        print(team.count)
+        team.append(Character(name: "Mook", gender: .female))
+        print("Please add another member to your team.")
+        } while team.count < 3
+        print("The team is ready to go.")
+    }
 }
