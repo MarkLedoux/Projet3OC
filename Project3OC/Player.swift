65ddf6111a3f12167fb665719557f8ba3473adb7
vinceled <mark.ledoux@live.fr>
2019-06-27 07:34:39 +0200

    func createTeam() created to display the number of teammates and if that number is below 3 add another teammate then display the names chosen

--- Project3OC/Player.swift
+++ Project3OC/Player.swift
@@ -10,8 +10,18 @@ import Foundation
 
 class Player {
     var name = ""
+    var team = [Character]()
 
     init(name: String) {
         self.name = name
+    }
+    func createTeam() {
+        repeat {
+            print(team.count)
+            print("Please add another member to your team.")
+            team.append(Character(name: readLine()!, gender: .female))
+        } while team.count < 3
+        print("Your team is ready to go.")
+        print("Your team is made up of \(team[0].name), \(team[1].name), \(team[2].name)")
     }
 }
--- Project3OC/main.swift
+++ Project3OC/main.swift
@@ -11,12 +11,16 @@ import Foundation
 var welcomeToGame = "Welcome to the Ultimate Fantasy Game!"
     print(welcomeToGame)
 
-var character1 = Werebison(name: "Paula", gender: .female)
+var player1 = Player(name: "Mark")
+player1.createTeam()
+
+/*var character1 = Werebison(name: "Paula", gender: .female)
 print("\(character1.name) has \(character1.healthPoints)")
 var character2 = Magician(name: "Pierre", gender: .male)
 print("\(character2.name) has \(character2.healthPoints)")
 
 character1.attack(target: character2)
 print("\(character2.name) has \(character2.healthPoints) HP left")
-
-
+character2.attack(target: character1)
+print("\(character1.name) has \(character1.healthPoints) HP left")
+*/
